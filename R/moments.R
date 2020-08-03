d832a8b3a0b0223c541a0ff4a86e2cef452eda9f
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-08-03 15:47:26 +0800

    Added mardia.

--- R/moments.R
+++ R/moments.R
@@ -1,4 +1,6 @@
-#' Central Moments
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Central Moments
 #'
 #' @description The sample central moment is defined by
 #' \deqn{
@@ -117,7 +119,6 @@
 #' - The third central moment is used to define skewness.
 #' - The fourth central moment is used to define kurtosis.
 #'
-#' @author Ivan Jacob Agaloos Pesigan
 #' @family moments functions
 #' @keywords moments
 #' @param x Numeric vector.
@@ -146,7 +147,9 @@ moment <- function(x, j) {
   }
 }
 
-#' Cumulant
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Cumulant
 #'
 #' @details
 #' \deqn{
@@ -179,7 +182,6 @@ moment <- function(x, j) {
 #'   \right\}
 #' }
 #'
-#' @author Ivan Jacob Agaloos Pesigan
 #' @family moments functions
 #' @keywords moments
 #' @inheritParams moment
@@ -238,7 +240,9 @@ b2 <- function(x) {
   (m4 / s^4) - 3
 }
 
-#' Kurtosis
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Kurtosis
 #'
 #' @description Calculates kurtosis
 #' which is a measure of the "taildness"
@@ -269,7 +273,6 @@ b2 <- function(x) {
 #'   %(\#eq:dist-moments-b2)
 #' }
 #'
-#' @author Ivan Jacob Agaloos Pesigan
 #' @family moments functions
 #' @keywords moments
 #' @inheritParams moment
@@ -311,14 +314,16 @@ kurt <- function(x, type = 2, excess = TRUE) {
     out <- b2(x)
   }
   if (excess) {
+    # g2, G2, and b2 already computes excess kurtosis
     return(out)
   } else {
     return(out + 3)
   }
 }
 
-
-#' Skewness
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Skewness
 #'
 #' @description Calculates kurtosis
 #' which is a measure of the asymmetry
@@ -349,7 +354,6 @@ kurt <- function(x, type = 2, excess = TRUE) {
 #'   %(\#eq:dist-moments-b1)
 #' }
 #'
-#' @author Ivan Jacob Agaloos Pesigan
 #' @family moments functions
 #' @keywords moments
 #' @inheritParams moment
@@ -387,4 +391,295 @@ skew <- function(x, type = 2) {
   if (type == 3) {
     return(b1(x))
   }
+}
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Compute mean, variance, standard deviation, skewness, and kurtosis
+#'
+#' @importFrom stats var sd
+#' @family moments functions
+#' @keywords moments
+#' @inheritParams moment
+#' @param type Integer.
+#'   1, 2, or 3.
+#'   See [`skew()`] and [`kurt()`].
+#' @export
+moments <- function(x, type = 2) {
+  c(
+    mean = mean(x = x),
+    var = var(x = x),
+    sd = sd(x = x),
+    skew = skew(x = x, type = type),
+    kurt = kurt(x = x, type = type)
+  )
+}
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Multivariate Skewness
+#'
+#' @description Mardia's estimate of multivariate skewness is given by
+#'   \deqn{
+#'     b_{1, k}
+#'     =
+#'     \frac{1}{n^2}
+#'     \sum_{i = 1}^{n}
+#'     \sum_{j = 1}^{n}
+#'     \left[
+#'       \left(
+#'         \mathbf{X}_{i} - \mathbf{\bar{X}}
+#'       \right)^{T}
+#'       \boldsymbol{\hat{\Sigma}}^{-1}
+#'       \left(
+#'         \mathbf{X}_{j} - \mathbf{\bar{X}}
+#'       \right)
+#'     \right]^{3}
+#'   }
+#'   where
+#'   - \eqn{\mathbf{X}} is the \eqn{n \times k} sample data
+#'   - \eqn{\mathbf{\bar{X}}} represent sample means
+#'   - \eqn{\mathbf{X}_{i} - \mathbf{\bar{X}}} and \eqn{\mathbf{X}_{j} - \mathbf{\bar{X}}} represent deviations from the mean
+#'   - \eqn{\boldsymbol{\hat{\Sigma}}} is the estimated variance-covariance matrix of \eqn{\mathbf{X}} using sample data
+#'
+#' @details If the null hypothesis that \eqn{\mathbf{X}} comes from a multivariate normal distribution is true,
+#'   \eqn{\frac{nb_{1, k}}{6}} follows a chi-square \eqn{\left( \chi^2 \right)} distribution with a df of \eqn{\frac{k(k + 1)(k + 2)}{6}} .
+#'
+#' @importFrom stats pchisq cov
+#' @importFrom jeksterslabRmatrix is.positive.definite
+#' @param X Matrix or data frame.
+#' @references
+#' Mardia, K. V. (1970).
+#' Measures of multivariate skewness and kurtosis with applications.
+#' *Biometrika*, *57*(3), 519-530.
+#' doi:[10.2307/2334770](https://doi.org/10.2307/2334770).
+#'
+#' Mardia, K. V. (1974).
+#' Applications of Some Measures of Multivariate Skewness and Kurtosis in Testing Normality and Robustness Studies.
+#' *Sankhyā: The Indian Journal of Statistics, Series B (1960-2002)*, *36*(2), 115-128.
+#' @return Returns a vector with the following elements
+#' \describe{
+#'   \item{b1}{Estimate of multivariate skewness \eqn{\left( b_{1, k} \right)} .}
+#'   \item{chisq}{chi-square statistic \eqn{\left( \frac{nb_{1, k}}{6} \right)} .}
+#'   \item{df}{Degrees of freedom \eqn{\left( \frac{k(k + 1)(k + 2)}{6} \right)} .}
+#'   \item{p}{p-value associated with the chi-square statistic.}
+#' }
+#' @examples
+#' set.seed(42)
+#' n <- 100
+#' mu <- c(0, 0, 0)
+#' Sigma <- matrix(
+#'   data = c(1, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 1),
+#'   ncol = 3
+#' )
+#' X <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
+#' mardiaskew(X)
+#' @export
+mardiaskew <- function(X) {
+  Sigma <- cov(X)
+  if (!is.positive.definite(X = Sigma, stop = FALSE)) {
+    stop(
+      "cov(X) is non-positive definite."
+    )
+  }
+  n <- nrow(X)
+  k <- ncol(X)
+  deviation <- scale(
+    X,
+    center = TRUE,
+    scale = FALSE
+  )
+  invSigma <- solve(Sigma)
+  # for loop version----------------------------------------------------------------
+  #  deviation <- scale(
+  #  X,
+  #  center = TRUE,
+  #  scale = FALSE
+  # )
+  # term <- matrix(
+  #  data = NA,
+  #  ncol = n,
+  #  nrow = n
+  # )
+  # for (j in 1:n) {
+  #  for (i in 1:n) {
+  #    term[i, j] <- (deviation[i, ] %*% invSigma %*% as.matrix(deviation[j, ]))^3
+  #  }
+  # }
+  # b1 <- (1 / n^2) * sum(term)
+  # vectorized version-----------------------------------------------------------------
+  term <- deviation %*% invSigma %*% t(deviation)
+  b1 <- (1 / n^2) * sum(term^3)
+  chisq <- n * (b1 / 6)
+  df <- k * (k + 1) * (k + 2) / 6
+  p <- pchisq(
+    q = chisq,
+    df = df,
+    lower.tail = FALSE
+  )
+  c(
+    b1 = b1,
+    chisq = chisq,
+    df = df,
+    p = p
+  )
+}
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Multivariate Kurtosis
+#'
+#' @description Mardia's estimate of multivariate kurtosis is given by
+#'   \deqn{
+#'     b_{2, k}
+#'     =
+#'     \frac{1}{n}
+#'     \sum_{i = 1}^{n}
+#'     \left[
+#'       \left(
+#'         \mathbf{X}_{i} - \mathbf{\bar{X}}
+#'       \right)^{T}
+#'       \boldsymbol{\hat{\Sigma}}^{-1}
+#'       \left(
+#'         \mathbf{X}_{i} - \mathbf{\bar{X}}
+#'       \right)
+#'     \right]^{2}
+#'   }
+#'   where
+#'   - \eqn{\mathbf{X}} is the \eqn{n \times k} sample data
+#'   - \eqn{\mathbf{\bar{X}}} represent sample means
+#'   - \eqn{\mathbf{X}_{i} - \mathbf{\bar{X}}} represent deviations from the mean
+#'   - \eqn{\boldsymbol{\hat{\Sigma}}} is the estimated variance-covariance matrix of \eqn{\mathbf{X}} using sample data
+#'
+#' @details If the null hypothesis that \eqn{\mathbf{X}} comes from a multivariate normal distribution is true,
+#'   \eqn{b_{2, k}} follows a normal distribution with a mean of \eqn{k \left( k + 2 \right)}
+#'   and a variance of \eqn{\frac{8 k \left( k + 2 \right)}{n}}.
+#'   Consequently,
+#'   \deqn{
+#'     \frac{b_{2, k} - k \left( k + 2 \right)}{\sqrt{\frac{8 k \left( k + 2 \right)}{n}}}
+#'   }
+#'   asymptotically follows a standard normal distribution \eqn{\mathcal{N} \left( 0, 1 \right)} .
+#'
+#' @importFrom stats pnorm
+#' @inheritParams mardiaskew
+#' @inherit mardiaskew references
+#' @return Returns a vector with the following elements
+#' \describe{
+#'   \item{b2}{Estimate of multivariate kurtosis \eqn{\left( b_{2, k} \right)} .}
+#'   \item{z}{z-statistic \eqn{\left( \frac{b_{2, k} - k \left( k + 2 \right)}{\sqrt{\frac{8 k \left( k + 2 \right)}{n}}} \right)} .}
+#'   \item{p}{p-value associated with the z-statistic.}
+#' }
+#' @examples
+#' set.seed(42)
+#' n <- 100
+#' mu <- c(0, 0, 0)
+#' Sigma <- matrix(
+#'   data = c(1, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 1),
+#'   ncol = 3
+#' )
+#' X <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
+#' mardiakurt(X)
+#' @export
+mardiakurt <- function(X) {
+  Sigma <- cov(X)
+  if (!is.positive.definite(X = Sigma, stop = FALSE)) {
+    stop(
+      "cov(X) is non-positive definite."
+    )
+  }
+  n <- nrow(X)
+  k <- ncol(X)
+  deviation <- scale(
+    X,
+    center = TRUE,
+    scale = FALSE
+  )
+  invSigma <- solve(Sigma)
+  # for loop version----------------------------------------------------------------
+  #  deviation <- scale(
+  #  X,
+  #  center = TRUE,
+  #  scale = FALSE
+  # )
+  # term <- rep(
+  #  x = NA,
+  #  length = n
+  # )
+  # for (i in 1:n) {
+  #  term[i] <- (deviation[i, ] %*% invSigma %*% as.matrix(deviation[i, ]))^2
+  # }
+  # b2 <- (1 / n) * sum(term)
+  # vectorized version-----------------------------------------------------------------
+  term <- deviation %*% invSigma %*% t(deviation)
+  b2 <- (1 / n) * sum(diag(term^2))
+  z <- (b2 - ((k * (k + 2)))) / sqrt(8 * k * ((k + 2) / n))
+  p <- 2 * pnorm(q = -abs(z))
+  c(
+    b2 = b2,
+    z = z,
+    p = p
+  )
+}
+
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Multivariate Skewness and Kurtosis
+#' @inheritParams mardiaskew
+#' @return Returns a vector with the following elements
+#' \describe{
+#'   \item{b1}{Estimate of multivariate skewness \eqn{\left( b_{1, k} \right)} .}
+#'   \item{b1.chisq}{chi-square statistic \eqn{\left( \frac{nb_{1, k}}{6} \right)} .}
+#'   \item{b1.df}{Degrees of freedom \eqn{\left( \frac{k(k + 1)(k + 2)}{6} \right)} .}
+#'   \item{b1.p}{p-value associated with the chi-square statistic.}
+#'   \item{b2}{Estimate of multivariate kurtosis \eqn{\left( b_{2, k} \right)} .}
+#'   \item{b2.z}{z-statistic \eqn{\left( \frac{b_{2, k} - k \left( k + 2 \right)}{\sqrt{\frac{8 k \left( k + 2 \right)}{n}}} \right)} .}
+#'   \item{b2.p}{p-value associated with the z-statistic.}
+#' }
+#' @examples
+#' set.seed(42)
+#' n <- 100
+#' mu <- c(0, 0, 0)
+#' Sigma <- matrix(
+#'   data = c(1, 0.5, 0.5, 0.5, 1, 0.5, 0.5, 0.5, 1),
+#'   ncol = 3
+#' )
+#' X <- MASS::mvrnorm(n = n, mu = mu, Sigma = Sigma)
+#' mardia(X)
+#' @export
+mardia <- function(X) {
+  Sigma <- cov(X)
+  if (!is.positive.definite(X = Sigma, stop = FALSE)) {
+    stop(
+      "cov(X) is non-positive definite."
+    )
+  }
+  n <- nrow(X)
+  k <- ncol(X)
+  deviation <- scale(
+    X,
+    center = TRUE,
+    scale = FALSE
+  )
+  invSigma <- solve(Sigma)
+  term <- deviation %*% invSigma %*% t(deviation)
+  b1 <- (1 / n^2) * sum(term^3)
+  b1.chisq <- n * (b1 / 6)
+  b1.df <- k * (k + 1) * (k + 2) / 6
+  b1.p <- pchisq(
+    q = b1.chisq,
+    df = b1.df,
+    lower.tail = FALSE
+  )
+  b2 <- (1 / n) * sum(diag(term^2))
+  b2.z <- (b2 - ((k * (k + 2)))) / sqrt(8 * k * ((k + 2) / n))
+  p.b2 <- 2 * pnorm(q = -abs(b2.z))
+  c(
+    b1 = b1,
+    b1.chisq = b1.chisq,
+    b1.df = b1.df,
+    b1.p = b1.p,
+    b2 = b2,
+    b2.z = b2.z,
+    p.b2 = p.b2
+  )
 }
